f4b6981557426d4dd29c52c2a5a73145777f93b2
lcdTain <lcd@18798828717@163.com>
2016-11-02 19:11:21 +0800

    登录成功闪屏获取数据保存到内存

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SplashActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SplashActivity.java
@@ -7,19 +7,25 @@ import android.view.animation.AlphaAnimation;
 import com.hyphenate.chat.EMClient;
 import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.bean.User;
+import cn.ucai.superwechat.db.UserDao;
+import cn.ucai.superwechat.utils.L;
 
 /**
  * 开屏页
  *
  */
 public class SplashActivity extends BaseActivity {
+	private static final String TAG = SplashActivity.class.getSimpleName();
 
 	private static final int sleepTime = 2000;
+	SplashActivity mContext;
 
 	@Override
 	protected void onCreate(Bundle arg0) {
 		setContentView(R.layout.em_activity_splash);
 		super.onCreate(arg0);
+		mContext = this;
 
 		/*RelativeLayout rootLayout = (RelativeLayout) findViewById(R.id.splash_root);
 		TextView versionText = (TextView) findViewById(R.id.tv_version);*/
@@ -41,6 +47,10 @@ public class SplashActivity extends BaseActivity {
 					long start = System.currentTimeMillis();
 					EMClient.getInstance().groupManager().loadAllGroups();
 					EMClient.getInstance().chatManager().loadAllConversations();
+					UserDao dao = new UserDao(mContext);
+					User user = dao.getUser(EMClient.getInstance().getCurrentUser());
+					L.e(TAG,"user="+user);
+					SuperWeChatHelper.getInstance().setCurrentUser(user);
 					long costTime = System.currentTimeMillis() - start;
 					//wait
 					if (sleepTime - costTime > 0) {
