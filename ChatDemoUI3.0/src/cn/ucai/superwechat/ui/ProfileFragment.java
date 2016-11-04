cf620d5ddb071998f4826202be9baebd865efbaa
lcdTain <lcd@18798828717@163.com>
2016-11-04 11:40:56 +0800

    添加个人中心的fragment

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ProfileFragment.java
@@ -0,0 +1,80 @@
+package cn.ucai.superwechat.ui;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.easemob.redpacketui.utils.RedPacketUtil;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.superwechat.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileFragment extends Fragment {
+
+
+    @Bind(R.id.iv_profile_avatar)
+    ImageView ivProfileAvatar;
+    @Bind(R.id.tv_profile_nickname)
+    TextView tvProfileNickname;
+    @Bind(R.id.tv_profile_username)
+    TextView tvProfileUsername;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.getBoolean("isConflict",false));
+        setUserInfo();
+    }
+
+    private void setUserInfo() {
+        EaseUserUtils.setCurrentAppUserAvatar(getActivity(),ivProfileAvatar);
+        EaseUserUtils.setCurrentAppUserNick(tvProfileNickname);
+        EaseUserUtils.setCurrentAppUserNameWithNo(tvProfileUsername);
+    }
+
+    @OnClick({R.id.layout_profile_view, R.id.tv_profile_money, R.id.tv_profile_settings})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.layout_profile_view:
+                break;
+            //red packet code : 进入零钱页面
+            case R.id.tv_profile_money:
+                RedPacketUtil.startChangeActivity(getActivity());
+                break;
+            case R.id.tv_profile_settings:
+                break;
+        }
+    }
+}
