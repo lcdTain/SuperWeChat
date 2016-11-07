98f8eb0848657d41124816b251738d900b563dbc
lcdTain <18798828717@163.com>
2016-11-07 16:22:43 +0800

    添加跳转到添加好友界面的方法

--- ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import java.util.ArrayList;
 
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.AddContactActivity;
 import cn.ucai.superwechat.ui.LoginActivity;
 import cn.ucai.superwechat.ui.MainActivity;
 import cn.ucai.superwechat.ui.RegisterActivity;
@@ -46,6 +47,9 @@ public class MFGT {
     }
     public static void gotoUserProfile(Activity context){
         startActivity(context, UserProfileActivity.class);
+    }
+    public static void gotoAddFrient(Activity context){
+        startActivity(context, AddContactActivity.class);
     }
 
 }
