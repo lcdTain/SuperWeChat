52d1b289e89a975624e33aee67b3d01f6bcf3696
lcdTain <18798828717@163.com>
2016-11-10 14:40:20 +0800

    联系人列表不显示自己

--- easeui/src/com/hyphenate/easeui/ui/EaseContactListFragment.java
+++ easeui/src/com/hyphenate/easeui/ui/EaseContactListFragment.java
@@ -319,7 +319,9 @@ public class EaseContactListFragment extends EaseBaseFragment {
      */
     public void setContactsMap(Map<String, User> contactsMap){
         this.contactsMap = contactsMap;
+        contactsMap.remove(EMClient.getInstance().getCurrentUser());
     }
+
     
     public interface EaseContactListItemClickListener {
         /**
